c22de8ebd46b473dd4935e1080cb95eee131aebc
JASHEN <oxxxyversus@gmail.com>
2022-10-08 10:32:51 +0900

    ReactTS from lesson

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Technologies from './Technologies';
+import Header from './Header';
 
-function App() {
+const App = () =>{
   return (
     <div className="App">
-     Hello, samurai! Let's go!
+        <Header />
+        <Technologies />
     </div>
   );
 }
