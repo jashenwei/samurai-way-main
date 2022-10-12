495a829232f2b7498abe2c081d10e195bb9c0188
JASHEN <oxxxyversus@gmail.com>
2022-10-12 09:20:15 +0900

    ReactTS from lesson

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Technologies from './Technologies';
 import Header from './Header';
+import Footer from "./Footer";
 
 const App = () =>{
   return (
     <div className="App">
         <Header />
+        <Footer />
         <Technologies />
     </div>
   );
